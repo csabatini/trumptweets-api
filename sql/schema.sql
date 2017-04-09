12e15516742f1cad429d42e9ea747373609bd97c
Caleb Sabatini <calebsabatini@gmail.com>
2017-04-09 15:18:12 -0500

    sqlite tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -18,7 +18,27 @@ CREATE TABLE status (
 );
 
 -- trumptweets.db
+CREATE TABLE status (
+     status_id TEXT NOT NULL PRIMARY KEY,
+     text TEXT NOT NULL,
+     created_at INTEGER NOT NULL,
+     UNIQUE(status_id)
+);
 
+CREATE TABLE tag (
+     tag_id INTEGER NOT NULL PRIMARY KEY,
+     tag TEXT NOT NULL,
+     UNIQUE(tag_id)
+);
+
+CREATE TABLE status_tag (
+     status_tag_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     status_id TEXT NOT NULL,
+     tag_id INTEGER NOT NULL,
+     FOREIGN KEY (status_id) REFERENCES status(status_id),
+     FOREIGN KEY (tag_id) REFERENCES tag(tag_id),
+     UNIQUE(status_id, tag_id)
+);
 
 -- trumptweets mysql
 CREATE DATABASE trumptweets CHARACTER SET utf8 COLLATE utf8_general_ci;
